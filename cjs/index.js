f0b2792939fc7bd3df078e80e99903c7f06c0e9a
franciscoblancojn <blancofrancisco34@gmail.com>
2025-11-19 09:19:20 -0500

    flb

---
+++ cjs/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A = void 0;
+exports.A = 1;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const A = 1
+export const A = 1;
